c6337d1d34de87350bd72a85339a02879896d37d
Tyler Mello <tmello001@gmail.com>
2023-05-06 18:18:01 -0400

    adding group issue test to test file

--- test/group4-tests/individual-issues.test.js
+++ test/group4-tests/individual-issues.test.js
@@ -1,8 +1,18 @@
-const assert = require('assert')
-const math = require('math')
+import assert from 'assert'
+import * as mainAny from '../../src/entry/mainAny.js'
+import * as factoriesAny from '../../src/factoriesAny.js'
+import { createSnapshotFromFactories, validateBundle, validateTypeOf } from '../../src/utils/snapshot.js'
+const { create, all, add, matrix, isObject, isMatrix, pi, speedOfLight, sqrt, evaluate, chain, reviver, Complex, addDependencies } = mainAny
 
 describe("Testing help('not') throwing an error", function () {
   it('should no longer throw a syntax error when running just help(not) alone', function () {
-    assert.doesNotThrow(() => math.evaluate('help(not)'), Error)
+    assert.doesNotThrow(() => evaluate('help(not)'), Error)
+  })
+})
+
+describe("Testing unit version precedence over function", function () {
+  it("should evaluate 'min' to minute, and 'sec' to second, while still supporting min() and sec() functions", function() {
+    assert.strictEqual(typeof evaluate('min'), 'object')
+    assert.strictEqual(typeof evaluate('sec'), 'object')
   })
 })
