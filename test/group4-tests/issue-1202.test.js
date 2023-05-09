31b7a6a511e82a7008810c8eb05ad0d5420f10ae
Tyler Mello <tmello001@gmail.com>
2023-05-09 10:34:29 -0400

    fixing linting issues

--- test/group4-tests/issue-1202.test.js
+++ test/group4-tests/issue-1202.test.js
@@ -1,20 +1,22 @@
 import assert from 'asssert'
+import * as mainAny from '../../src/entry/mainAny.js'
+const { evaluate, parse, expression } = mainAny
 
-describe('mathjs parser', function() {
-  it('should parse single-quoted strings as string literals with stringNumber option', function() {
-    const input1 = "'Hello, world!'";
+describe('mathjs parser', function () {
+  it('should parse single-quoted strings as string literals with stringNumber option', function () {
+    const input1 = "'Hello, world!'"
     const options1 = {
-      stringNumber: true,
-    };
-    const result1 = parse(input1, options1);
-    assert.deepEqual(result1, new expression.node.ConstantNode('Hello, world!'));
+      stringNumber: true
+    }
+    const result1 = parse(input1, options1)
+    assert.deepEqual(result1, new expression.node.ConstantNode('Hello, world!'))
 
-    const input2 = "'2''";
+    const input2 = "'2''"
     const options2 = {
-      stringNumber: true,
-    };
-    const result2 = parse(input2, options2);
-    assert.deepEqual(result2, new expression.node.ConstantNode('2\''));
-    assert.equal(evaluate(input2), "2'"); // Use math.evaluate to verify that the parsed expression evaluates correctly
+      stringNumber: true
+    }
+    const result2 = parse(input2, options2)
+    assert.deepEqual(result2, new expression.node.ConstantNode('2\''))
+    assert.equal(evaluate(input2), "2'") // Use math.evaluate to verify that the parsed expression evaluates correctly
   });
 });
